bb0f89dc0f2ebfbff8c248ce9163d8e4a9c250b8
mh-anwar <mohammad.anwar2939@gmail.com>
2022-04-26 22:53:12 -0400

    Small modification

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Link } from 'react-router-dom';
 
 function App() {
-  return <div className="App">g</div>;
+  return <div className="App">Under construction</div>;
 }
 
 export default App;
